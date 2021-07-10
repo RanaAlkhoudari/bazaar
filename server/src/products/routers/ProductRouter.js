1d68d638639f6c6e31ef3c0cb54bf232cc18b793
Rana <rana.alkhoudari@gmail.com>
2021-07-10 15:46:51 +0200

    added status

--- server/src/products/routers/ProductRouter.js
+++ server/src/products/routers/ProductRouter.js
@@ -6,14 +6,20 @@ const router = express.Router();
 
 router.get('/products/:id', (req, res) => {
   Product.findById(req.params.id)
-    .then((product) => res.json(product))
+    .then((product) => {
+      res.status(200);
+      res.json(product);
+    })
     .catch((err) => res.status(400).json(`Error :  ${err}`));
 });
 
 router.get('/products', (req, res) => {
   const { category } = req.query;
   Product.find({ category })
-    .then((product) => res.json(product))
+    .then((product) => {
+      res.status(200);
+      res.json(product);
+    })
     .catch((err) => res.status(400).json(`Error :  ${err}`));
 });
 
