937d86ccad2736865484b9d5f19daf7bcb458332
Rana <rana.alkhoudari@gmail.com>
2021-07-10 13:40:32 +0200

    remove comment from server.js

--- server/src/server.js
+++ server/src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { loadDb } = require('./utils/db');
 const { loadRepositories } = require('./middlewares/repositories');
 const { handleErrors } = require('./middlewares/errors');
-// const userRouter = require('./users/router');
+const userRouter = require('./users/router');
 
 const app = express();
 
