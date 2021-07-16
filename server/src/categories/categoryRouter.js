aedb080a7e832c341df2a484ebdeaf5f076744b3
Deedee <dorsnet@hotmail.com>
2021-07-16 16:50:26 +0200

    Show categories controller

--- server/src/categories/categoryRouter.js
+++ server/src/categories/categoryRouter.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const CreateCategory = require('./controllers/createCategory');
+const ShowCategory = require('./controllers/showCategory');
 
 const router = express.Router();
 
 router.post('/create', CreateCategory);
+router.get('/', ShowCategory);
 
 module.exports = router;
