8d9ac3b6bd95ee6355c927ceb2e008398e534c43
Mahmood <pejvakco@gmail.com>
2021-07-22 12:42:13 +0200

    CheckouPage first div

--- client/src/pages/checkout/checkout.js
+++ client/src/pages/checkout/checkout.js
@@ -8,10 +8,24 @@ const Checkout = (prop) => {
   return (
     <div className={styles.checkoutWrapper}>
       <div className={styles.leftContainer}>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div className={styles.firstDiv}>
+          <div className={styles.productImageContainer}>
+            <img src={product.images[0]} alt="Product Image" />
+          </div>
+          <div>
+            <h3>{product.title}</h3>
+            <h5>{product.price} €</h5>
+            <p>{product.description}</p>
+          </div>
+        </div>
         <div>
+          <h3>Product Information</h3>
+        </div>
+        <div>
+          <h3>Shipping Address</h3>
+        </div>
+        <div>
+          <h3>Credit Card Info</h3>
           <Payment />
         </div>
       </div>
