f6b4126c449ebdd35c6a9fc7eb6ad1e5d9170b0e
Mahmood <pejvakco@gmail.com>
2021-07-24 09:59:26 +0200

    Add shipping input box

--- client/src/pages/checkout/checkout.js
+++ client/src/pages/checkout/checkout.js
@@ -36,11 +36,34 @@ const Checkout = (prop) => {
             <p>{product.description}</p>
           </div>
         </div>
-        <div>
+        {/* <div>
           <h3>Product Information</h3>
-        </div>
-        <div>
+        </div> */}
+        <div className={styles.shippingContainer}>
           <h3>Shipping Address</h3>
+          <div className="row">
+            <div className="col-md-6">
+              <input type="text" name="first_name" placeholder="First Name" />
+            </div>
+            <div className="col-md-6">
+              <input type="text" name="last_name" placeholder="Last Name" />
+            </div>
+            <div className="col-md-12">
+              <input type="text" name="city" placeholder="City" />
+            </div>
+            <div className="col-md-4">
+              <input type="text" name="post_code" placeholder="PostCode" />
+            </div>
+            <div className="col-md-4">
+              <input type="text" name="building_number" placeholder="Building Number" />
+            </div>
+            <div className="col-md-4">
+              <input type="text" name="extension" placeholder="Extra" />
+            </div>
+            <div className="col-md-12">
+              <input type="text" name="street_name" placeholder="Street Name" />
+            </div>
+          </div>
         </div>
         <div>
           <h3>Credit Card Info</h3>
@@ -48,7 +71,11 @@ const Checkout = (prop) => {
         </div>
       </div>
       <div className={styles.rightContainer}>
-        <img src={seller.avatar} alt="User image" />
+        {/* <img src={seller.avatar} alt="User image" /> */}
+        <img
+          src="https://www.tele2.nl/blog/wp-content/uploads/2020/08/facebook-avatar-maken-tips-tele2.jpg"
+          alt="User image"
+        />
         <h3>
           {seller.first_name} {seller.last_name}
         </h3>
