20e2f2357516085ee180365017291cc3bbd34bc8
Wael Kiwan <waelkiwan88@gmail.com>
2021-07-27 16:43:01 +0200

    fixed .env path

--- server/src/seeding.js
+++ server/src/seeding.js
@@ -8,7 +8,7 @@ const dummyCategories = require('./dummy-files/categories.json');
 const dummyOrders = require('./dummy-files/orders.json');
 
 if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
+  require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 }
 console.log(process.env.DB_URL);
 
