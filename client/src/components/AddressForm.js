f8a52598f576ca003d8fcd5566cf7a657152b639
Wael Kiwan <waelkiwan88@gmail.com>
2021-07-30 09:14:03 +0200

    added user can add new address on checkout

--- client/src/components/AddressForm.js
+++ client/src/components/AddressForm.js
@@ -1,43 +1,146 @@
-import React from 'react';
+import axios from 'axios';
 import { Card, Col, Row } from 'react-bootstrap';
+import React, { useContext, useRef } from 'react';
+import { AuthContext } from '../context/AuthContext';
 
-const AddressForm = () => {
+const AddressForm = ({ getNewAddress }) => {
+  const { user } = useContext(AuthContext);
+
+  const cityRef = useRef();
+  const countryRef = useRef();
+  const lastNameRef = useRef();
+  const postCodeRef = useRef();
+  const extensionRef = useRef();
+  const firstNameRef = useRef();
+  const streetNameRef = useRef();
+  const buildingNumberRef = useRef();
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+
+    const newAddress = {
+      user: user._id,
+      city: cityRef.current.value || null,
+      last_name: lastNameRef.current.value,
+      extension: extensionRef.current.value,
+      first_name: firstNameRef.current.value,
+      country: countryRef.current.value || null,
+      post_code: postCodeRef.current.value || null,
+      street_name: streetNameRef.current.value || null,
+      building_number: buildingNumberRef.current.value || null,
+    };
+
+    // TODO: change (input) to (Form.Control) to be able to set (required) attribute and remove that (isFilled())
+    const isFilled = () => {
+      for (const key in newAddress) if (newAddress[key] === null) return false;
+      return true;
+    };
+
+    if (isFilled()) {
+      const response = await axios.post(
+        `${process.env.REACT_APP_API_URL}/addresses/create`,
+        newAddress,
+      );
+
+      // set it on the parent component (addressesDropdown) if the user filled all the required fields
+      getNewAddress(response.data.address);
+    }
+  };
+
   return (
     <Card style={cardStyles}>
       <Card.Header>Shipping Address</Card.Header>
       <Card.Body>
         <Row>
           <Col md={6}>
-            <input style={inputStyles} type="text" name="first_name" placeholder="First Name" />
+            <input
+              type="text"
+              name="first_name"
+              ref={firstNameRef}
+              style={inputStyles}
+              placeholder="First Name"
+            />
           </Col>
+
           <Col md={6}>
-            <input style={inputStyles} type="text" name="last_name" placeholder="Last Name" />
+            <input
+              type="text"
+              name="last_name"
+              ref={lastNameRef}
+              style={inputStyles}
+              placeholder="Last Name"
+            />
           </Col>
         </Row>
+
         <Row>
-          <Col md={12}>
-            <input style={inputStyles} type="text" name="city" placeholder="City" />
+          <Col md={6}>
+            <input
+              type="text"
+              name="street_name"
+              style={inputStyles}
+              ref={streetNameRef}
+              placeholder="Street Name"
+            />
           </Col>
-        </Row>
-        <Row>
-          <Col md={4}>
-            <input style={inputStyles} type="text" name="post_code" placeholder="PostCode" />
-          </Col>
+
           <Col md={4}>
             <input
-              style={inputStyles}
               type="text"
+              style={inputStyles}
               name="building_number"
+              ref={buildingNumberRef}
               placeholder="Building Number"
             />
           </Col>
-          <Col md={4}>
-            <input style={inputStyles} type="text" name="extension" placeholder="Extra" />
+
+          <Col md={2}>
+            <input
+              type="text"
+              name="extension"
+              ref={extensionRef}
+              style={inputStyles}
+              placeholder="Extension (Optional)"
+            />
           </Col>
         </Row>
+
+        <Row>
+          <Col md={4}>
+            <input
+              type="text"
+              name="post_code"
+              ref={postCodeRef}
+              style={inputStyles}
+              placeholder="Post Code"
+            />
+          </Col>
+
+          <Col md={4}>
+            <input type="text" name="city" ref={cityRef} placeholder="City" style={inputStyles} />
+          </Col>
+
+          <Col md={4}>
+            <input
+              type="text"
+              name="country"
+              ref={countryRef}
+              style={inputStyles}
+              placeholder="Country"
+            />
+          </Col>
+        </Row>
+
         <Row>
           <Col md={12}>
-            <input style={inputStyles} type="text" name="street_name" placeholder="Street Name" />
+            <input
+              value="Add"
+              type="submit"
+              name="submit"
+              style={btnStyles}
+              onClick={handleClick}
+              className="btn btn-primary"
+            />
           </Col>
         </Row>
       </Card.Body>
@@ -45,14 +148,18 @@ const AddressForm = () => {
   );
 };
 
-const inputStyles = {
+const cardStyles = {
   marginBottom: '10px',
-  width: '100%',
+  borderColor: 'var(--color-main)',
 };
 
-const cardStyles = {
-  borderColor: 'var(--color-main)',
+const inputStyles = {
+  width: '100%',
   marginBottom: '10px',
+};
+
+const btnStyles = {
+  background: 'var(--color-main)',
 };
 
 export default AddressForm;
