91d85268f288c32fab8d3fabdcb64971410830a4
Wael Kiwan <waelkiwan88@gmail.com>
2021-08-12 09:43:34 +0200

    fixed removed wrong prop

--- client/src/components/GoogleSignIn.js
+++ client/src/components/GoogleSignIn.js
@@ -12,26 +12,29 @@ const GoogleSignIn = () => {
   const { dispatch } = useContext(AuthContext);
 
   async function responseGoogle(response) {
-    const res = await axios({
-      method: 'POST',
-      url: `${process.env.REACT_APP_API_URL}/users/googleLogin`,
-      data: {
+    try {
+      const data = {
         accessToken: response.accessToken,
-        userID: response.Zb.id_token,
+        userID: response.id_token,
         email: response.profileObj.email,
-      },
-    });
+      };
 
-    function loginCall(userCredential, dispatch) {
-      dispatch({ type: 'LOGIN_START' });
-      try {
-        dispatch({ type: `LOGIN_SUCCESS`, payload: res.data });
-        history.push('/');
-      } catch (err) {
-        dispatch({ type: `LOGIN_FAILURE`, payload: err.response.data });
+      const res = await axios.post(`${process.env.REACT_APP_API_URL}/users/googleLogin`, data);
+
+      function loginCall(userCredential, dispatch) {
+        dispatch({ type: 'LOGIN_START' });
+        try {
+          dispatch({ type: `LOGIN_SUCCESS`, payload: res.data });
+          history.push('/');
+        } catch (err) {
+          dispatch({ type: `LOGIN_FAILURE`, payload: err.response.data });
+        }
       }
+      loginCall({ email: res.data.email, password: res.data.password }, dispatch);
+    } catch (err) {
+      console.log(err)
     }
-    loginCall({ email: res.data.email, password: res.data.password }, dispatch);
+
   }
   return (
     <Card style={{ width: '350px' }}>
